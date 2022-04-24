e14e071549a3b16fc168bb0cffeef0d5f9a8a24e
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-23 21:46:09 -0400

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Navbar from "./components/Navbar"
 import React from "react"
 
 export default function App () {
+    const [searching, setSearching] = React.useState(true);
+
+    function handleNavigation (){
+        setSearching(prevSearching=>!prevSearching);
+    }
+
+
     return (
-        <h1>Test</h1>
+        <div className="main-container">
+            <Navbar
+                searching={searching}
+                handleClick={handleNavigation}
+            />
+        </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
-// import React from "react"
+import React from "react"
 // import Logo from "../images/logo.png"
 
-export default function Navbar() {
+export default function Navbar(props) {
+
     return (
         <nav>
-            
+           <h1>{props.searching? "Find Your Film" : "My Watchlist"}</h1>
+           <h3 onClick={props.handleClick}>{props.searching? "My Watchlist" : "Find Your Film"}</h3>
         </nav>
     )
 }
