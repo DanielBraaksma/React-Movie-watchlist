5d7bfbacd4d6fc8df864798bf2325469a92155ab
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-28 20:44:20 -0400

    begin styling movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,16 @@
 import React from "react"
+import Staricon from "../images/staricon.png"
+import Addicon from "../images/addicon.png"
 
-export default function Movie (props) {
+export default function Movie(props) {
     const [movieData, setMovieData] = React.useState([])
 
 
 
     React.useEffect(() => {
-    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=ad56d582&t=${props.data}`)
-        .then(res=>res.json())
-        .then(data=> setMovieData(data))
+        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=ad56d582&t=${props.data}`)
+            .then(res => res.json())
+            .then(data => setMovieData(data))
 
 
     }, [props])
@@ -16,8 +18,17 @@ export default function Movie (props) {
     console.log(movieData)
     return (
         <div className="movie-card">
-            <p>{movieData.Title}</p>
-            <img src={movieData.Poster} />
+            <img className="movie-img" src={movieData.Poster} />
+            <div className="movie-info">
+                <p className="movie-title">{movieData.Title}</p>
+                <span className="rating-container"><img src={Staricon} />{movieData.imdbRating}</span>
+
+                <div className="subtitle">
+                    <p>{movieData.Year}</p><p>{movieData.Genre}</p>
+                    <button className="addBtn"><img src={Addicon} />Add to Watchlist</button>
+                </div>
+                <p>{movieData.Plot}</p>
+            </div>
         </div>
     )
 }
