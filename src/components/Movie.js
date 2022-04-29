5087f39ce2289137900bfabf125520a7dd3ceed2
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-28 21:34:06 -0400

    completed style to movie card

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Staricon from "../images/staricon.png"
-import Addicon from "../images/addicon.png"
+import Addicon from "../images/testicon.png"
 
 export default function Movie(props) {
     const [movieData, setMovieData] = React.useState([])
@@ -20,12 +20,12 @@ export default function Movie(props) {
         <div className="movie-card">
             <img className="movie-img" src={movieData.Poster} />
             <div className="movie-info">
-                <p className="movie-title">{movieData.Title}</p>
-                <span className="rating-container"><img src={Staricon} />{movieData.imdbRating}</span>
+                <h2 className="movie-title">{movieData.Title}</h2>
+                <span className="rating-container"><img src={Staricon} className="star" />{movieData.imdbRating}</span>
 
                 <div className="subtitle">
                     <p>{movieData.Year}</p><p>{movieData.Genre}</p>
-                    <button className="addBtn"><img src={Addicon} />Add to Watchlist</button>
+                    <button className="addBtn"><img src={Addicon} />ADD</button>
                 </div>
                 <p>{movieData.Plot}</p>
             </div>
